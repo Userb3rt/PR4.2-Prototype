640fc36225f252839fe608547ccba4fcf79de0e0
Roberto Zambrano <rzajr868@gmail.com>
2023-10-03 20:30:30 +0200

    lista+listacopia

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    List<Electrodomestic>listaeElectrodomestics = new ArrayList<>();
+    List<Electrodomestic>listaCopy = new ArrayList<>();
+
     public static void main(String[] args) {
     }
 }
